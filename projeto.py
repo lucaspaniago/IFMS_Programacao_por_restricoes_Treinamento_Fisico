adda19dbe83b53fb2f416e1934fa0dc39591087d
“Lucas <lucaspaniago10@gmail.com>
2020-04-04 01:25:20 -0400

    adição de variáveis de restrição da lista de exercícios

--- projeto.py
+++ projeto.py
@@ -2,139 +2,215 @@ import random
 from ortools.sat.python import cp_model
 
 
-def isnumber(digito):
+def ehNumero(digito):
     try:
          int(digito)
     except ValueError:
          return False
     return True
 
-def calculaTempoDeExecucao(velocidadeDaExecucao):
+def calculaTempoDeExecucao(velocidadeDeExecucao):
 
-    return sum([int(x) for x in velocidadeDaExecucao if isnumber(x)])
+    return sum([int(x) for x in velocidadeDeExecucao if ehNumero(x)])
 
 '''
     Variáveis que o usuário irá inserir
 '''
-quantidadeDeExercicios = 3
+quantidadeDeExercicios = 10
 numeroDeRepeticoesPadrao = 12
 velocidadeDeExecucaoPadrao = '2020'
 tempoDeExecucaoPadrao = calculaTempoDeExecucao(velocidadeDeExecucaoPadrao) * numeroDeRepeticoesPadrao
-
+restricoes = {
+    'objetivo': 'hipertrofia',
+    'nivel': 'iniciante'
+}
 
 listaDeExercicios = [
     {
         'id': 1,
         'nome': 'CRUCIFIXO DECLINADO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 1,
+        'iniciante': 0
     },
     {
         'id': 2,
         'nome': 'CRUCIFIXO RETO COM ELÁSTICO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 1,
+        'iniciante': 1
     },
     {
         'id': 3,
         'nome': 'CRUCIFIXO DECLINADO COM ELÁSTICO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 1,
+        'iniciante': 0
     },
     {
         'id': 4,
         'nome': 'CRUCIFIXO RETO CROSS OVER EM PÉ',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 5,
         'nome': 'PULLOVER NO BANCO COM HALTER',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 1,
+        'iniciante': 1
     },
     {
         'id': 6,
         'nome': 'CRUCIFIXO EM PÉ INCLINADO CROSS OVER',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 1,
+        'iniciante': 0
     },
     {
         'id': 7,
         'nome': 'CRUCIFIXO FITA DE SUSPENSÃO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 8,
         'nome': 'APOIO FITA DE SUSPENSÃO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 1
     },
     {
         'id': 9,
         'nome': 'PRESSÃO ARCO MÁGICO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 1
     },
     {
         'id': 10,
         'nome': 'PARTIDA COM O PEITO NO CHÃO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 11,
         'nome': 'SUPINO RETO ALTERNADO HALTER',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 12,
         'nome': 'SUPINO RETO FECHADO ALTERNADO HALTER',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 1
     },
     {
         'id': 13,
         'nome': 'SUPINO INCLINADO FECHADO ALTERNADO HALTER',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 14,
         'nome': 'SUPINO FECHADO BARRA',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 1
     },
     {
         'id': 15,
         'nome': 'SUPINO RETO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 1
     },
     {
         'id': 16,
         'nome': 'SUPINO DECLINADO ALTERNADO HALTER',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 17,
         'nome': 'SUPINO DECLINADO FECHADO ALTERNADO HALTER',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 18,
         'nome': 'APOIO FECHADO FITA DE SUSPENSÃO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 19,
         'nome': 'APOIO COM BOSU INVERTIDO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 20,
         'nome': 'APOIO NO BOSU',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 21,
         'nome': 'APOIO SOLO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
     {
         'id': 22,
         'nome': 'APOIO DECLINADO',
         'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 1,
+        'flexibilidade': 0,
+        'iniciante': 0
     },
+    {
+        'id': 23,
+        'nome': 'APOIO SOLO JOELHO NO CHÃO',
+        'tempo': tempoDeExecucaoPadrao,
+        'hipertrofia': 0,
+        'flexibilidade': 0,
+        'iniciante': 1
+    }
 ]
-
 
 
 
@@ -144,10 +220,10 @@ listaDeExercicios = [
 
 model = cp_model.CpModel()
 
-vars = []
+variaveis = []
 
-for ex in listaDeExercicios:
-    vars.append(model.NewIntVar(0, 1, "{}".format(ex['nome'])))
+for exercicio in listaDeExercicios:
+    variaveis.append(model.NewIntVar(0, 1, "{}".format(exercicio['nome'])))
 
 '''
     ::..Seção 2 - Definição das restrições..::
@@ -157,14 +233,37 @@ for ex in listaDeExercicios:
     Como ainda não tem restrições pelas necessidades dos alunos, gero aleatóriamente a lista
     de exercícios selecionados
 '''
-indices = random.sample(range(len(listaDeExercicios)), quantidadeDeExercicios)
-exerciciosSelcionados = [listaDeExercicios[i] for i in indices]
+exerciciosSelecionados = []
 
+if len(restricoes) > 0:
+    status = True
+
+    for exercicio in listaDeExercicios:
+        status = True
+        for restricao in restricoes:
+            if not exercicio[restricoes[restricao]]:
+                status = False
+        
+        if status:
+            exerciciosSelecionados.append(exercicio)
+
+    while len(exerciciosSelecionados) < quantidadeDeExercicios:
+        quantidadeDeExercicios = int(input(f"Existem apenas {len(exerciciosSelecionados)} exercicios que satisfazem as exigências. Diminua a quantidade de exercícios para este músculo.\nDigite a nova quantidade: "))
+        
+    indices = random.sample(range(len(exerciciosSelecionados)), quantidadeDeExercicios)
+    exerciciosSelecionados = [exerciciosSelecionados[i] for i in indices]
+else:
+    indices = random.sample(range(len(listaDeExercicios)), quantidadeDeExercicios)
+    exerciciosSelecionados = [listaDeExercicios[i] for i in indices]
+    
+
+# for i in exerciciosSelecionados:
+#     print(i)
 '''
     Acho que modularizar a contrução, criando uma lista de exercícios para cada músculo
     será mais organizado, eficaz e viável. Por isso tornei a variável abaixo genérica
 '''
-exercicios = [vars[i] for i in range(quantidadeDeExercicios)]
+exercicios = [variaveis[i] for i in range(quantidadeDeExercicios)]
 exerciciosTotal = sum(exercicios)
 
 '''
@@ -172,7 +271,7 @@ exerciciosTotal = sum(exercicios)
     depois, com a lista de exercícios completa.
 '''
 #tempoTotal = vars[0] * listaDeExercicios[0]['tempo'] + vars[1] * listaDeExercicios[1]['tempo']
-tempos = [vars[i] * listaDeExercicios[i]['tempo'] for i in range(len(vars))]
+tempos = [variaveis[i] * listaDeExercicios[i]['tempo'] for i in range(len(variaveis))]
 tempoTotal = sum(tempos)
 
 # Caso, realizar o treino no menor tempo possível, seja uma exigência
@@ -189,9 +288,9 @@ model.Add(exerciciosTotal == quantidadeDeExercicios)
 solver = cp_model.CpSolver()
 status = solver.Solve(model)
 
-for i in range(len(vars)):
-    if solver.Value(vars[i]):
-        print (exerciciosSelcionados[i]['nome'])
+for i in range(len(variaveis)):
+    if solver.Value(variaveis[i]):
+        print (exerciciosSelecionados[i]['nome'])
 '''
 
 if solver.Value(vars[0]):
